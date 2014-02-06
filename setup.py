6c88e0dde1ac00885e286cf9a8f7cef637dbb82b
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-05 16:56:22 -0800

    Update package configuration

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     name='tangled.contrib',
     version='0.1.dev0',
     description='Tangled namespace for contributed packages',
+    long_description=open('README.rst').read(),
+    url='http://tangledframework.org/',
+    author='Wyatt Baldwin',
+    author_email='self@wyattbaldwin.com',
     packages=find_packages(),
     install_requires=(
         'tangled>=0.1.dev0',
