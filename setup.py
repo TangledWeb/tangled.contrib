fb5d3ee88d7e62412288142cccc94891cabfe593
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-09 16:47:55 -0800

    Prepare release 0.1a3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled.contrib',
-    version='0.1a3.dev0',
+    version='0.1a3',
     description='Tangled namespace for contributed packages',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
